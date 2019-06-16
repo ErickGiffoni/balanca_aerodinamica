2e8fb97decbb647a142e7c162156f342423cc5dc
MoacirMSJ <48539765+MoacirMSJ@users.noreply.github.com>
2019-06-16 16:27:52 -0300

    Update balanca_aero.ino

--- balanca_aero.ino
+++ balanca_aero.ino
@@ -1,6 +1,11 @@
 #include "HX711.h"
-//#define pinos de saida
-//#define pinos de entrada
+
+//pinos de entrada
+#define
+#define
+//pinos de clock
+#define
+#define
 
 float fator_de_calibracao1 = 1000;
 float fator_de_calibracao2 = 1000;
@@ -8,22 +13,56 @@ float fator_de_calibracao2 = 1000;
 HX711 balanca_aero;
 HX711 balanca_aero2;
 
+//dodos para as funçoes de arrasto e 
+double fg = 9.81; // m/s^2 - aceleracao gravitacional
+double corda = 0.08; // m - 8 cm
+double densidade_do_fluido = 1.225; // kg/m^3
+double velocidade = 10.0; // m/s
+double velocidade_2 = velocidade*velocidade // mˆ2/sˆ2 - velocidade ao quadrado
+double massa_da_celula = 0.0; // kg
+
+//Assinatura das funcoes
+double coef_sustentacao(double *massa_da_celula);
+double coef_arrasto(double *massa_da_celula);
+
 void setup() {
   Serial.begin(9600);
-//  balanca_aero.begin(pino saida,pino entrada);
-//  balanca_aero2.begin(pino saida,pino entrada);
+
+  balanca_aero.begin(pino entrada1,pino clock1);
+  balanca_aero2.begin(pino entrada2,pino clock2);
+  
   Serial.print("\t\tcomeçou\n");
+  
+  //define o fator de calibracao
   balanca_aero.set_scale(fator_de_calibracao1);
   balanca_aero2.set_scale(fator_de_calibracao2);
   
 }
 
 void loop() {
+  //zera a balanca
   balanca_aero.tare();
   balanca_aero2.tare();
-  float peso1 = balanca_aero.get_unit();
-  float peso2 = balanca_aero2.get_unit();
+  
+  //get_units realizara a media dos 5 valores de  massas medidas 
+  float massa1 = balanca_aero.get_units(5);
+  float massa2 = balanca_aero2.get_units(5);
 
-//  chamar as funçoes
-// peso, arrasto..... passando peso como parametro
+  //chamada das funcoes
+  double sustentacao = coef_sustentacao(&massa1);
+  double arrasto = coef_arrasto(&massa2);
+
+  //imprime os coeficientes
+  Serial.print("\nCoeficiente de sustentacao: ")
+  Serial.print(sustentacao);
+  Serial.print("\nCoeficiente de arrasto: ")
+  Serial.print(arrasto);
+
+}
+double coef_sustentacao(double *massa_da_celula){
+  return ((*massa_da_celula)*(fg)*(0.5)*(densidade_do_fluido)*(velocidade_2)*(corda));
+}
+
+double coef_arrasto(double *massa_da_celula){
+  return ((*massa_da_celula)*(0.5)*(densidade_do_fluido)*(velocidade_2)*(corda));
 }
